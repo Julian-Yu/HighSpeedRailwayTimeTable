eb8c87820d3c04a31edfcfbec1adce1c15c547b2
JulianYu <16251244@bjtu.edu.cn>
2022-01-24 16:52:33 +0800

    完善DM类

--- DataManager.cs
+++ DataManager.cs
@@ -9,24 +9,38 @@ namespace HighSpeedRailwayTimeTable
     internal class DataManager
     {
         /// <summary>
-        /// 线路列表
+        /// 线路字典
         /// 键与线路编号Line.line_no相同
         /// </summary>
         public Dictionary<int, Line> LineDict = new Dictionary<int, Line>();
         /// <summary>
-        /// 列车列表
+        /// 列车字典
         /// 键与线路编号Train.train_no相同
         /// </summary>
         public Dictionary<int, Train> TrainDict = new Dictionary<int, Train>();
         /// <summary>
-        /// 列车列表
+        /// 列车字典
         /// 键与线路编号Train.train_no相同
         /// </summary>
         public Dictionary<int, Train> UpTrainDict = new Dictionary<int, Train>();
         /// <summary>
-        /// 列车列表
+        /// 列车字典
         /// 键与线路编号Train.train_no相同
         /// </summary>
         public Dictionary<int, Train> DownTrainDict = new Dictionary<int, Train>();
+        /// <summary>
+        /// 从外部文件中读取线路字典
+        /// </summary>
+        public void ReadLineDict()
+        {
+
+        }
+        /// <summary>
+        /// 从外部文件中读取列车字典
+        /// </summary>
+        public void ReadTrainDict()
+        {
+
+        }
     }
 }
